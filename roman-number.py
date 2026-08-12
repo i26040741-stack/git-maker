7cbc4f4427315fa4b079844db6d0bc3bc02b1f63
MADING JORDAN ZUNG YING <i26040741@student.newinti.edu.my>
2026-08-12 09:11:00 +0800

    Add files via upload

---
+++ roman-number.py
@@ -0,0 +1,25 @@
+roman_values = [
+    (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I")
+]
+
+result = ""
+
+answer = int(input("Enter the numbers: "))
+
+for value,symbol in roman_values:
+    while answer >= value:
+        answer -= value
+        result += symbol
+print("roman:", result)
